c54c245e2303af8ea1add211662731077c4b470a
julius-boettger <boettger.julius@gmail.com>
2023-03-12 17:20:44 +0100

    added logging to script

--- install_firefox_dev.sh
+++ install_firefox_dev.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# escape codes for colorful output
+NC="\e[0m" # no color
+RED="\e[31m"
+GREEN="\e[32m"
+#printf "${RED}red${NC}\r\n"
+#printf "${GREEN}green${NC}\r\n"
+
 # make sure curl and tar are installed
+printf "installing curl and tar...\r\n\n"
 sudo apt install curl
+printf "\r\n"
 sudo apt install tar
+printf "\n${GREEN}curl and tar installed.${NC}\r\n\n"
+
+printf "downloading firefox developer edition. "
 
 # set $LOCALE to first argument
 LOCALE="$1"
@@ -10,19 +22,28 @@ LOCALE="$1"
 # set $LOCALE to default value if there are no arguments
 if [ $# -eq 0 ]; then
     LOCALE="en-US"
+    printf "no locale received, using default value \"${LOCALE}\"...\r\n\n"
+else
+    printf "using first argument \"${LOCALE}\" as locale...\r\n\n"
 fi
 
 # download firefox archive to /opt/
 sudo curl --location "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=${LOCALE}" --output /opt/firefox.tar.bz2
 
+printf "\n${GREEN}downloaded archive.${NC}\r\n\n"
+
+printf "expanding archive...\r\n"
 # expand the archive in /opt/
 sudo tar -xjf /opt/firefox.tar.bz2 -C /opt/
+printf "${GREEN}archive expanded.${NC}\r\n\n"
 
 # delete the archive
 sudo rm -rf /opt/firefox.tar.bz2
+printf "${GREEN}deleted archive.${NC}\r\n"
 
 # make current user owner of firefox/
 sudo chown -R $USER /opt/firefox
+printf "${GREEN}made \"$(whoami)\" owner of /opt/firefox.${NC}\r\n"
 
 # write shortcut file
 cat > ~/.local/share/applications/firefox_dev.desktop << EOF
@@ -40,6 +61,11 @@ StartupWMClass=firefox-aurora
 EOF
 # end shortcut file
 
+printf "${GREEN}wrote shortcut file.${NC}\r\n"
+
 # mark shortcut file as trusted and make it executable
 chmod +x ~/.local/share/applications/firefox_dev.desktop
+printf "${GREEN}marked shortcut as trusted and made it executable.${NC}\r\n"
+
+printf "\n${GREEN}installation complete.${NC}\r\n"
 
